2a5a6fee86dcf5c84d8d0e696c842ca28af9000b
Gabe Firestone <Swicano@users.noreply.github.com>
2020-01-13 12:44:53 -0500

    HelloWorld
    made basic console project, which says hello, echoes back an input string, then reads in a string, parses it into float, and outputs it.

---
+++ Hello/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string echoLine = Console.ReadLine();
+            float testNumber = 1.2f;
+            Console.WriteLine($"You wrote: {echoLine } and testNumber is {testNumber }");
+            Console.WriteLine("Input a new Number:");
+            string inputLine = Console.ReadLine();
+            testNumber = float.Parse(inputLine);
+            System.Console.WriteLine($"you entered: {testNumber }");
+            
+
+
+        }
+    }
+}
+
